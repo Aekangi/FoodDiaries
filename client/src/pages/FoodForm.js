e06aa33e16c49400efcbb9065dc22dde5e6c9e7f
Aekangi Patel <aekangi91@gmail.com>
2022-10-24 12:16:45 -0400

    response comes in console

--- client/src/pages/FoodForm.js
+++ client/src/pages/FoodForm.js
@@ -1,9 +1,33 @@
-import './App.css'
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
 const FoodForm = () => {
   const [recipe, setRecipe] = useState([])
+
+  const initialForm = {
+    name: '',
+    difficulty_Level: '',
+    time: '',
+    servings: '',
+    ingredients: '',
+    directions: '',
+    image: ''
+  }
+
+  const [formState, setFormState] = useState(initialForm)
+
+  useEffect(() => {
+    const getFoods = async () => {
+      try {
+        let response = await axios.get('http://localhost:3001/foods')
+        setRecipe(response.data)
+        console.log(response.data)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+    getFoods()
+  }, [])
 }
 
 export default FoodForm
