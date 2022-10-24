0912cabdee935a0dbf20b16f1dc9c8998332dc62
Aekangi Patel <aekangi91@gmail.com>
2022-10-24 11:52:29 -0400

    set initial state of foodForm

---
+++ client/src/pages/FoodForm.js
@@ -0,0 +1,9 @@
+import './App.css'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const FoodForm = () => {
+  const [recipe, setRecipe] = useState([])
+}
+
+export default FoodForm
