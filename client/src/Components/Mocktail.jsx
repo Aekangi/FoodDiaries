c7f9f026593f20241bdc5d6bcd94d9216495ba28
Aekangi Patel <aekangi91@gmail.com>
2022-10-25 11:22:44 -0400

    mocktail component card complete

---
+++ client/src/Components/Mocktail.jsx
@@ -0,0 +1,24 @@
+
+
+
+const Mocktail = (props) => {
+    return (
+    <div className="card" onClick={props.onClick}>
+        <div className="img-wrapper">
+        <img src={props.image} alt='' />
+        </div>
+        <div className="info-wrapper flex-col">
+            <h2>{props.name}</h2>
+            <h5>{props.difficulty_Level}</h5>
+            <p>
+            {props.time}
+            {props.servings}
+            {props.ingredients}
+            {props.directions}
+            </p>
+        </div>
+    </div>
+    )
+}
+
+export default Mocktail
