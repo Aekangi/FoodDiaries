78f1d6e523af3325f70c7956b0ee369ef07ff840
Aekangi Patel <aekangi91@gmail.com>
2022-10-30 15:48:02 -0400

    cheese

--- client/src/pages/Foods.js
+++ client/src/pages/Foods.js
@@ -9,7 +9,7 @@ const Foods = () => {
 
   const getFood = async () => {
     try {
-      const response = await axios.get('api/foods')
+      const response = await axios.get('/api/foods')
       setFoods(response.data.foods)
     } catch (err) {
       console.log(err)
@@ -21,7 +21,7 @@ const Foods = () => {
 
   const deleteFoodEntry = async (id) => {
     try {
-      const response = await axios.delete(`/foods/${id}`)
+      const response = await axios.delete(`/api/foods/${id}`)
       getFood(response)
     } catch (err) {
       console.log(err)
